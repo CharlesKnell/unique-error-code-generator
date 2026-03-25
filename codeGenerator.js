7707ff8462700674b203e3a6691d33f01854f725
Charles Knell <charles1283@gmail.com>
2026-03-25 08:09:01 -0500

    present unique code as a base 62 number to shorten it

--- codeGenerator.js
+++ codeGenerator.js
@@ -2,10 +2,21 @@ function generateErrorCode() {
     return crypto.randomUUID().replaceAll('-', '').toUpperCase();
 }
 
+function hexToBase62(hex) {
+    const CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let num = BigInt('0x' + hex);
+    if (num === 0n) return '0';
+    let result = '';
+    while (num > 0n) {
+        result = CHARS[Number(num % 62n)] + result;
+        num /= 62n;
+    }
+    return result;
+}
+
 let codeElement = document.getElementById('errorCode');
 let submitButton = document.getElementById('submit');
-
 submitButton.addEventListener('click', () => {
-  let uniqueCode = generateErrorCode();
-  codeElement.innerText = uniqueCode;
+    let uniqueCode = generateErrorCode();
+    codeElement.innerText = hexToBase62(uniqueCode);
 });
